e3ca6e4023b5bca72ead59d52fb2552098bbd96d
cybermist2 <cybermist2@gmail.com>
2024-03-07 21:51:03 +0200

    add project

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
